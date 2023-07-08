4ee0ef86598d082828a15408a56041fe37eeaf2f
MichalAdryanczyk <118079627+MichalAdryanczyk@users.noreply.github.com>
2023-07-08 19:47:12 +0000

    Deploying to gh-pages from @ MichalAdryanczyk/goit-js-hw-09@b55ab5fadff9c4ec6e94e9cdfb527c699f7e6004 🚀

---
+++ 01-color-switcher.131e2d39.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]");document.querySelector("body");let d=null;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.131e2d39.js.map
